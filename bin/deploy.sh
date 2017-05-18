47202e4afa80c6573c28269fc17e42fe744e627f
Lorenz Bischof <lorenzbischof@gmail.com>
2017-05-18 16:15:23 +0200

    install plugins in build step

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,5 @@ set -ex
 if [[ -n "${OPENPROJECT_MANAGED_REPOS}" ]]; then
     ln -snf /opt/app-root/src/config/configuration.yml /opt/app-root/etc/configuration.yml
 fi
-bundle install
 bundle exec rake db:migrate
 #RAILS_ENV=production bundle exec rake generate_secret_token
-
