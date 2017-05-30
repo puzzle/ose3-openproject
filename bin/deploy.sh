7d8b55f207d4e349e5d89080f4507d6f089d032d
Lorenz Bischof <lorenzbischof@gmail.com>
2017-05-30 17:48:46 +0200

    install plugins after build

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,6 @@ set -ex
 if [[ -n "${OPENPROJECT_MANAGED_REPOS}" ]]; then
     ln -snf /opt/app-root/src/config/configuration.yml /opt/app-root/etc/configuration.yml
 fi
+bundle
 bundle exec rake db:migrate
 #RAILS_ENV=production bundle exec rake generate_secret_token
