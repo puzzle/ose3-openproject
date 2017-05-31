6b7f963e2d5c0f5f2c56f269fbc355d8006429a0
Lorenz Bischof <lorenzbischof@gmail.com>
2017-05-31 10:12:17 +0200

    unset env var

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,5 +4,7 @@ if [[ -n "${OPENPROJECT_MANAGED_REPOS}" ]]; then
     ln -snf /opt/app-root/src/config/configuration.yml /opt/app-root/etc/configuration.yml
 fi
 bundle
+# still set to sqlite from buildconfig (?)
+unset DATABASE_URL
 bundle exec rake db:migrate
 #RAILS_ENV=production bundle exec rake generate_secret_token
